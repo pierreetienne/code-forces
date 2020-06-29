5cb97e9e3f77dcaa0b8e3e7a930238027a104bd4
Pierre E <etienne.pradere@gmail.com>
2020-06-29 11:21:46 -0500

    solution

---
+++ src/CF979A.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CF979A {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(in.readLine());
+        String data = in.readLine() + "%";
+        int ans = 0;
+        int last = -1;
+        int count = 0;
+        for (int i = 0; i < data.length(); ++i) {
+            if (last !=-1 && last == 'x' && data.charAt(i) == 'x') {
+                count++;
+            } else {
+                count++;
+                if (count >= 3) {
+                    ans += count - 2;
+                }
+                count = 0;
+            }
+            last = data.charAt(i);
+        }
+
+        sb.append(ans).append("\n");
+
+        System.out.print(new String(sb));
+
+    }
+}
