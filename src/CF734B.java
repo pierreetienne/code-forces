a7257306a6c38ffbc5ab7593c2da6c5591a90b9f
Pierre Etienne <etienne.pradere@gmail.com>
2020-10-24 10:53:13 -0500

    changes

--- src/CF734B.java
+++ src/CF734B.java
@@ -14,15 +14,12 @@ public class CF734B {
         int min256 = Math.min(a, Math.min(c,d));
         int ans = min256*256;
         int a1 = a-min256;
-        int c1 = c-min256;
-        int d1 = d-min256;
         int min32 = Math.min(a1, b);
         if(min32 > 0){
             ans+=min32*32;
         }
         min32 = Math.min(a,b);
         a1 = a-min32;
-        int b1 = b-min32;
         int ans2 = min32*32;
         min256 = Math.min(a1,Math.min(c,d));
         if(min256 > 0){
